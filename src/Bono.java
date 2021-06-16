f1e2999a89403965ab67f34c7ee8c71dbf4fd2c0
Jhony <Jhony.fuentes19@gmail.com>
2021-06-16 14:26:18 -0600

    Ejercicio bono

--- src/Bono.java
+++ src/Bono.java
@@ -12,9 +12,9 @@ public class Bono {
     private String Nombre;
     private int Edad;
     private int Dni;
-    private char Sexo = SEXO_DEFECTO;
+    private char Sexo;
     private double Peso;
-    private float Altura;
+    private double Altura;
 
     public static void main(String[] args){
         Scanner leer = new Scanner(System.in);
@@ -22,7 +22,7 @@ public class Bono {
         int Edad;
         char Sexo;
         double Pesoo;
-        float Alturaa;
+        double Alturaa;
 
         leer.nextLine();
         System.out.println("Ingrese el nombre");
@@ -30,51 +30,82 @@ public class Bono {
         System.out.println("Ingrese la edad");
         Edad = leer.nextInt();
         System.out.println("Ingrese el sexo");
-        Sexo = leer.next();
+        Sexo = leer.next().charAt(0);
         System.out.println("Ingrese el peso");
         Pesoo = leer.nextDouble();
         System.out.println("Ingrese la altura");
         Alturaa = leer.nextFloat();
 
-        Bono Persona1 = new Bono();
-        Bono Persona2 = new Bono();
+        Bono Persona1 = new Bono(Nombree,Edad,Sexo,Pesoo,Alturaa);
+        Bono Persona2 = new Bono(Nombree,Edad,Sexo);
         Bono Persona3 = new Bono();
+        Persona3.setAltura(1.75);
+        Persona3.setEdad(24);
+        Persona3.setSexo('M');
+        Persona3.setPeso(90);
+        Persona3.setNombre("Carlos");
+
+        System.out.println("Comprobando Peso Ideal");
+        Persona1.CalcularIMC();
+        Persona2.CalcularIMC();
+        Persona3.CalcularIMC();
+
+        System.out.println("\nComprobando mayor de edad");
+        Persona1.EsMayor();
+        Persona2.EsMayor();
+        Persona3.EsMayor();
+
+        System.out.println("\nMostrando Datos de las 3 personas");
+        Persona1.toStringg();
+        Persona2.toStringg();
+        Persona3.toStringg();
+
 
     }
 
     public Bono(){
-
+        this.Nombre = "";
+        this.Edad = 0;
+        this.Sexo = SEXO_DEFECTO;
+        this.Peso = 0;
+        this.Altura = 0;
+        this.Dni = 0;
     }
 
     public Bono(String Nom,int Edad,char sexo){
         this.Nombre = Nom;
-        thos.Edad = Edad;
+        this.Edad = Edad;
         this.Sexo = sexo;
-
+        this.Peso = 1;
+        this.Altura = 1;
+        this.Dni = 0;
     }
 
-    public Bono(String Nom,int Edad,char sexo,int dni,double peso,float Altura){
+    public Bono(String Nom,int Edad,char sexo,double peso,double Altura){
         this.Nombre = Nom;
-        thos.Edad = Edad;
+        this.Edad = Edad;
         this.Sexo = sexo;
         this.Peso = peso;
         this.Altura = Altura;
-        this.Dni = dni;
+        this.Dni = GenerarDni();
 
     }
 
     public int CalcularIMC(){
         int Imc;
-        int Estado;
-        Imc = Peso/Altura*Altura;
+        int Estado = 0;
+        Imc = (int)Peso/(int)Altura*(int)Altura;
         if(Imc<20){
             Estado = PESO_IDEAL;
+            System.out.println(Nombre + "tu peso es: Peso Ideal");
         }
         if(Imc>=20 & Imc<=25){
             Estado = POR_DEBAJO;
+            System.out.println(Nombre + " Tu Peso esta por debajo del ideal");
         }
         if(Imc>25){
             Estado = SOBREPESO;
+            System.out.println(Nombre + " tienes sobre peso, haz ejercicio");
         }
 
         return Estado;
@@ -84,9 +115,11 @@ public class Bono {
         boolean EsMayor;
         if(Edad >= 18){
             EsMayor = true;
+            System.out.println(Nombre + " Es mayor de edad");
         }
         else{
             EsMayor = false;
+            System.out.println(Nombre + " No es mayor de edad");
         }
 
         return EsMayor;
@@ -130,7 +163,7 @@ public class Bono {
         this.Edad = Edad;
     }
 
-    public void setAltura(float Altura){
+    public void setAltura(double Altura){
         this.Altura = Altura;
     }
 
